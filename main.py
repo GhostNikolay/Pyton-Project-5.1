0fc7633620e4a68e712dae809c057ea49145acc7
GhostNikolay <snl-10@mail.ru>
2024-08-30 07:04:48 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,8 @@
+first_name =  12
+print(first_name)
+second_name= 1.5
+print(second_name)
+third_name= 'курс ' + 'Python'
+print(third_name)
+print(second_name  / first_name)
+print(third_name,  first_name, second_name, second_name / first_name)
